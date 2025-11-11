0789692fa026f669709a4efa9e78ac60a66bc757
Infinity-777333 <ashwameth777@gmail.com>
2025-11-11 21:36:38 +0530

    Add files via upload

---
+++ python_Ashwameth/31-built_in_packages.py
@@ -0,0 +1,13 @@
+import math
+import random
+import datetime
+
+random_number = random.randint(1, 100)
+print(f"Random number between 1 and 100: {random_number}")
+current_datetime = datetime.datetime.now()
+print(f"Current date and time: {current_datetime}")
+num=int(input("Enter a number:"))
+sqr=math.sqrt(num)
+print(f"The square root of {num} is {sqr}")
+    
+    
